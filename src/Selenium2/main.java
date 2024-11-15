9d92dfd8c66ca33b67fda610678e826a3f8ae69b
= <johanhartono@gmail.com>
2024-11-15 18:03:12 +0700

    added web driver with multiple browsers and options

---
+++ src/Selenium2/main.java
@@ -0,0 +1,72 @@
+// Please look at this references for browser options
+
+// https://stackoverflow.com/questions/51844033/how-to-correctly-use-firefoxoptions-and-its-arguments-to-pass-it-to-the-firefox
+// https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/firefox/FirefoxOptions.html
+// https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/chrome/ChromeOptions.html
+// https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/edge/EdgeOptions.html
+// https://www.selenium.dev/documentation/webdriver/browsers/safari/
+// https://www.selenium.dev/documentation/webdriver/browsers/internet_explorer/
+
+package Selenium2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import Setup.utils;
+
+public class main {
+
+	public static WebDriver driver;
+	public WebDriverWait wait;
+
+	@Test
+	@Parameters("browser")
+	public void main(String browser) throws Exception {
+
+		// 1.Define selected Browser Driver
+		if (browser.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--disable-search-engine-choice-screen");
+			options.addArguments(
+					"--disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints");
+			// options.addArguments("--headless"); // for github CLI/linux/others without
+			// GUI
+			driver = new ChromeDriver(options);
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			FirefoxOptions options = new FirefoxOptions();
+			options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+			driver = new FirefoxDriver(options);
+		} else if (browser.equalsIgnoreCase("edge")) {
+			EdgeOptions options = new EdgeOptions();
+			options.setBinary("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+			driver = new EdgeDriver(options);
+		} else if (browser.equalsIgnoreCase("ie")) {
+			InternetExplorerOptions options = new InternetExplorerOptions();
+			driver = new InternetExplorerDriver(options);
+		}
+
+		// 2.get Browser URL
+		driver.get(utils.webUrl);
+		driver.getTitle();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20));
+
+		// 3. Ending
+		Thread.sleep(10000);
+		driver.quit();
+	}
+
+}
